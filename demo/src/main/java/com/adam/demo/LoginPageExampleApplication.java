3ec01f3d4f81f435a1e3f8b37eb573365d69cdb1
adam <adben97@gmail.com>
2020-06-01 14:49:25 -0400

    First commit

---
+++ demo/src/main/java/com/adam/demo/LoginPageExampleApplication.java
@@ -0,0 +1,13 @@
+package com.adam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPageExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginPageExampleApplication.class, args);
+	}
+
+}
